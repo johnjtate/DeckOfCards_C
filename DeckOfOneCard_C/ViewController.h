53f7255fa5bc1ba801dffdf8d4357cb872770618
John Tate <johntate@Johns-MacBook-Pro.local>
2018-09-11 14:50:27 -0600

    Initial commit

---
+++ DeckOfOneCard_C/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DeckOfOneCard_C
+//
+//  Created by John Tate on 9/11/18.
+//  Copyright © 2018 John Tate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
